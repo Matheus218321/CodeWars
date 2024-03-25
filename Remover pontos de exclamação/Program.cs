019307ea4c649ef3a14e66cec290ffbc2a450f52
Matheus <Matheus218321@gmail.com>
2024-03-25 18:07:09 -0300

    Remover Exclamação

---
+++ Remover pontos de exclamação/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            string Texto = "";
+            Console.WriteLine("Digite um texto: ");
+            Texto = Console.ReadLine();
+            Console.WriteLine("Seu texto sem pontos de exclamação: " + RemoverExclamacao(Texto));
+        }
+    }
+
+    static string RemoverExclamacao(string Texto)
+    {
+        string TextoFinal = "";
+
+        for (int i = 0; i < Texto.Length; i++)
+        {
+            if (Texto[i].ToString() != "!")
+            {
+                TextoFinal += Texto[i];
+            }
+        }
+
+        return TextoFinal;
+    }
+}
