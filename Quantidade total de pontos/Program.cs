39c352e95aeccac98da6b84fe6950b271fe26e34
Matheus <Matheus218321@gmail.com>
2024-03-25 16:15:04 -0300

    Total de Pontos

---
+++ Quantidade total de pontos/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static int TotalPontos(string[] games)
+    {
+        int Total = 0;
+
+        foreach (string jogo in games)
+        {
+            string[] Pontuacao = jogo.Split(':');
+
+            int PontosCasa = Convert.ToInt32(Pontuacao[0]);
+            int PontosInimigo = Convert.ToInt32(Pontuacao[1]);
+
+            if (PontosCasa > PontosInimigo)
+            {
+                Total += 3;
+            }
+
+            if (PontosCasa == PontosInimigo)
+            {
+                Total += 1;
+            }
+
+        }
+
+        return Total;
+    }
+}
