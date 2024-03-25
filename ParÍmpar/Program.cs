490973560bddaaa566d6e9be638939ace2821e8f
Matheus <Matheus218321@gmail.com>
2024-03-25 14:29:13 -0300

    ParImpar

---
+++ ParÍmpar/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            int Numero = 0;
+            Console.WriteLine("Digite um número inteiro: ");
+            Numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Esse número é: " + ParImpar(Numero));
+        }
+    }
+
+    static string ParImpar(int Numero)
+    {
+        if (Numero % 2 == 0)
+        {
+            return "Par";
+        }
+
+        else
+        {
+            return "Ímpar";
+        }
+
+        return "";
+    }
+}
