42326a114158eb52821d82e1e86657fd746b0467
Matheus <Matheus218321@gmail.com>
2024-03-25 10:38:11 -0300

    Vogal

--- Substitua todas as vogais por ponto de exclamação na frase/obj/Debug/net8.0/Substitua todas as vogais por ponto de exclamação na frase.AssemblyInfo.cs
+++ Substitua todas as vogais por ponto de exclamação na frase/obj/Debug/net8.0/Substitua todas as vogais por ponto de exclamação na frase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Substitua todas as vogais por ponto de exclamação na frase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40a6811b2c3040703fb3c032f60a9d50614063cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+685c0d78b0bdc140368e5ed8d2f2fa6d95ebeb4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Substitua todas as vogais por ponto de exclamação na frase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Substitua todas as vogais por ponto de exclamação na frase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
