c478fd63a916b1842b3b02603cae890da2a2fd2a
Matheus <Matheus218321@gmail.com>
2024-03-26 19:26:28 -0300

    Código PIN

---
+++ Bug persistente/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(Persistence(39));
+    }
+
+    static int Persistence(long Numero)
+    {
+        long Soma = Numero;
+        int Multi = 1;
+        int Repeti = 0;
+        
+        while(Soma >= 10)
+        {
+            Multi = 1;
+
+            char[] Digito = Soma.ToString().ToCharArray();
+            
+            foreach(char Digitos in Digito)
+            {
+                Multi = Multi * (int)Char.GetNumericValue(Digitos);
+            }
+
+            Soma = Multi;
+            Repeti++;
+        }
+
+
+        return Repeti;
+    }
+}
---
+++ Regex valida código PIN/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+   static void Main()
+    {
+
+    }
+
+    public static bool ValidatePin(string pin)
+    {
+        char[] Digitos =  pin.ToCharArray();
+
+        if(Digitos.Length == 4 || Digitos.Length == 6)
+        {
+            foreach(char Digito in Digitos)
+            {
+                if(Char.IsDigit(Digito) == false)
+                {
+                    return false;
+                }
+            }
+        }
+
+        else
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
