7df7c1e79acd71108b314f5fea63465480216936
Matheus <Matheus218321@gmail.com>
2024-03-26 20:38:10 -0300

    Número de Telefone

---
+++ Criar número de telefone/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Frozen;
+
+class Program
+{
+    static void Main()
+    {
+
+    }
+
+    public static string CreatePhoneNumber(int[] numbers)
+    {
+        string NumeroTelefone = "";
+        char[] Caracteres = new char[numbers.Length];
+
+        for(int i = 0; i < numbers.Length; i++)
+        {
+            Caracteres[i] = Convert.ToChar(numbers[i] + '0');
+        }
+
+        NumeroTelefone = string.Format("({0}{1}{2}) {3}{4}{5}-{6}{7}{8}{9}", Caracteres[0], Caracteres[1], Caracteres[2], Caracteres[3], Caracteres[4], Caracteres[5], Caracteres[6], Caracteres[7], Caracteres[8], Caracteres[9]);
+        return NumeroTelefone;
+    }
+}
