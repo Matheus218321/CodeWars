8ad8b2d903d3d394b4216880b57abf8d2831a7d7
Matheus <Matheus218321@gmail.com>
2024-03-26 21:22:43 -0300

    Tempo Legível

---
+++ TestarSeFaz/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            int Segundos = 0;
+            Console.WriteLine("Digite os segundos: ");
+            Segundos = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Horário: " + GetReadableTime(Segundos));
+        }
+        
+    }
+
+    public static string GetReadableTime(int seconds)
+    {
+
+        string Horas = "";
+        int Hora = 0;
+        int Minuto = 0;
+        int Segundo = 0;
+        
+        Hora = seconds / 3600;
+        Minuto = (seconds % 3600) / 60;
+        Segundo = seconds % 60;
+
+        Horas = String.Format("{0:00}:{1:00}:{2:00}", Hora, Minuto, Segundo);
+
+        return Horas;
+    }
+}
